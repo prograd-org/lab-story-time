e2f9ed6428c75867316608d60aeadbdc2b4bfb2c
Dhanalashmi-K <61022242+Dhanalashmi-K@users.noreply.github.com>
2021-06-19 21:05:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,18 +8,15 @@ const moreAboutHome =(address, distanceFromTown, hasNeighbours) => {
     let hasNeighboursData = typeof hasNeighbours
     return addressData + distanceFromTownData + hasNeighboursData;
 }
-
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 const moreAboutKaren = (parents, noOfSiblings, isNuclearFamily) => {
-    let parentsData = typeof parents;
-    let noOfSiblingsData = typeof noOfSiblings;
-    let isNuclearFamilysData = typeof isNuclearFamily
-
-    return parentsData + noOfSiblingsData + isNuclearFamilysData === "stringnumberboolean";
+    let addressData = typeof parents;
+    let distanceFromTownData = typeof noOfSiblings;
+    let hasNeighboursData = typeof isNuclearFamily
+    return addressData + distanceFromTownData + hasNeighboursData === "stringnumberboolean";
 }
-
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
@@ -27,34 +24,37 @@ const moreAboutKaren = (parents, noOfSiblings, isNuclearFamily) => {
 const doesFriendExist = (parents, noOfSiblings) => {
     return (typeof parents === NaN || parents == '' ) ? NaN : parents;
 }
-
 //Progression 4:
 //Lily gave Karen x sweets
 //Karen ate y sweets herself
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
-const sweetTooth =(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) => {
-    let totalNoOfSweetsData = typeof totalNoOfSweets;
-    let sweetsConsumedByKarenData = typeof sweetsConsumedByKaren;
-    let sweetsConsumedInNMetersData = typeof sweetsConsumedInNMeters;
-    let metersToTravelData = typeof metersToTravel;
-    if (totalNoOfSweetsData+ sweetsConsumedByKarenData+ sweetsConsumedInNMetersData+ metersToTravelData !== "numbernumbernumbernumber"  || totalNoOfSweetsData+ sweetsConsumedByKarenData+ sweetsConsumedInNMetersData+ metersToTravelData === null) 
-    { return "No sweets for Karen's friend"; }
-    else if (totalNoOfSweetsData+ sweetsConsumedByKarenData+ sweetsConsumedInNMetersData+ metersToTravelData === 0) { return 0; }
-    return sweetsConsumedByKaren + ((totalNoOfSweets- (metersToTravel + sweetsConsumedByKaren)) /2 );
-  
+function sweetTooth (totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    if(
+        typeof totalNoOfSweets !== "number" ||
+        typeof sweetsConsumedByKaren !== "number" ||
+        typeof sweetsConsumedInNMeters !== "number" ||
+        typeof metersToTravel !== "number" ||
+        arguments.length == 0
+    ) 
+        return "No sweets for Karen's friend"
+    if(Array.from(arguments).filter(item => item <= 0).length == 0)
+        return 0;
+    let sweetsLeftAfterKarenAte = totalNoOfSweets - sweetsConsumedByKaren;
+    let sweetsAfterKarenAtePerMetres = sweetsLeftAfterKarenAte - sweetsConsumedInNMeters;
+    if(sweetsAfterKarenAtePerMetres > 0)
+        return Math.floor(sweetsAfterKarenAtePerMetres/2);
+    else if ( sweetsAfterKarenAtePerMetres <= 0 )
+        return 0;
 }
-
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
 const convertToCelsius = (fahrenheit) => {
-    let fahrenheitData = typeof fahrenheit;
-    return (5/9) * (fahrenheit -32);
+     return typeof fahrenheit !== "number" ? 
+     "Technical Error!" : (fahrenheit - 32) / 1.8;
 }
-
-
 //Progression 6:
 //Lily can now do multiple things to deal with this
 //1. Take her daughter to a doctor
@@ -63,15 +63,18 @@ const convertToCelsius = (fahrenheit) => {
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
 const aDifficultChoice = (choice) => {
-    let choiceData = typeof choice;
-    "1. Take her daughter to a doctor"
-    "2. Talk to her husband about it"
-    "3. Counsel her daughter herself"
-    "4. Lock her daughter in her room"
-    switch (
+    choice = choice === "I give up" ? 2 : choice;
+    switch (choice) {
+        case 1:
+            return "Take her daughter to a doctor";
+        case -1:
+            return "Break down and give up all hope";
+        case 2:
+            return "Refused to do anything for Karen"
+        default:
+            return "Wasn't able to decide";
+    }
 }
-
-
 //Progression 7:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
@@ -80,3 +83,5 @@ const aDifficultChoice = (choice) => {
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+const consoleKaren = (strategies) => {
+}
